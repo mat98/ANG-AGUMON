647f910318d79a87061967e8e24145fa353a945f
mat98 <mateusd.santos10@gmail.com>
2022-12-06 09:49:33 -0300

    ajustes na tela inicial informações primárias

--- src/app/pages/home/init/home-init.component.ts
+++ src/app/pages/home/init/home-init.component.ts
@@ -9,7 +9,7 @@ import { ProjectsHome } from '../projects.model';
 export class HomeInitComponent implements OnInit {
   projects = [
     {
-      nameProject: 'Chapmon',
+      nameProject: 'Chapmon Stocks',
       routeDetail: 'chapmon',
       img: 'https://cdn.dribbble.com/users/1044634/screenshots/14833409/media/9daaf6db84d0e5c8a4a4545db7ee4006.jpg?compress=1&resize=1000x750&vertical=top',
       description: 'Projeto criado para simular compra e venda de ações',
